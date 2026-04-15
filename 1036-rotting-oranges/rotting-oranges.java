9321991142d668f10f524ce8523619259940995b
Himavanth <muppirihimavanth2@gmail.com>
2026-04-15 11:00:27 +0530

    Time: 2 ms (90.31%) | Memory: 44.6 MB (8.37%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -4,29 +4,20 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         Queue<int[]> queue = new LinkedList<>();
         int fresh = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int rows = grid.length, cols = grid[0].length;
 
-        // Find rotten oranges and count fresh oranges
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 2) {
-                    queue.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
-                }
+                if (grid[i][j] == 2) queue.offer(new int[]{i, j});
+                else if (grid[i][j] == 1) fresh++;
             }
         }
-
-        if (fresh == 0) return 0;
 
         int minutes = 0;
         int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
-        // BFS
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty() && fresh > 0) {
             int size = queue.size();
-            boolean changed = false;
 
             for (int i = 0; i < size; i++) {
                 int[] curr = queue.poll();
@@ -39,12 +30,11 @@ class Solution {
                         grid[r][c] = 2;
                         fresh--;
                         queue.offer(new int[]{r, c});
-                        changed = true;
                     }
                 }
             }
 
-            if (changed) minutes++;
+            minutes++;
         }
 
         return fresh == 0 ? minutes : -1;
